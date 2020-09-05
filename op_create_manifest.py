f04c9148d727f60cb8913a943a418904d83f1d78
samytichadou <samytichadou@gmail.com>
2020-09-05 12:54:13 +0200

    create manifest iterate through folders

--- op_create_manifest.py
+++ op_create_manifest.py
@@ -2,11 +2,41 @@ import bpy
 import os
 
 
+# return direct subfolders
+def return_direct_subfolders(folderpath):
+
+    folder_paths = []
+
+    for item in os.listdir(folderpath):
+
+        item_path = os.path.join(folderpath, item)
+
+        if os.path.isdir(item_path):
+            folder_paths.append(item_path)
+
+    return folder_paths
+
+
+# return direct files in folder
+def return_direct_files(folderpath):
+
+    file_paths = []
+
+    for item in os.listdir(folderpath):
+
+        item_path = os.path.join(folderpath, item)
+
+        if os.path.isfile(item_path):
+            file_paths.append(item_path)
+
+    return file_paths
+
+
 class ANTEMPLATES_OT_create_manifest(bpy.types.Operator):
     """Create Manifest from Github URL and Template Folder"""
     bl_idname = "antemplates.create_manifest"
     bl_label = "Create Manifest"
-    bl_options = {'REGISTER', 'INTERNAL'}
+    bl_options = {'REGISTER'}#, 'INTERNAL'}
 
     github_url : bpy.props.StringProperty(name="Github URL")
 
@@ -29,6 +59,23 @@ class ANTEMPLATES_OT_create_manifest(bpy.types.Operator):
 
     def execute(self, context):
 
-    
+        categories = []
+        
+        for folder in return_direct_subfolders(self.template_folder):
+
+            # get categories
+            categories.append(os.path.basename(folder))
+            print(folder)
+
+            # get nodetrees
+            for subfolder in return_direct_subfolders(folder):
+                
+                for filepath in return_direct_files(subfolder):
+
+                    if os.path.basename(filepath) == "readme.md":
+                        readme_file = filepath
+                        # create json entry for the nodetree
+                        break
+
 
         return {'FINISHED'}
