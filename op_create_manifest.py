64c0ec853f50a087dce76ad69d89019ef78fd73a
samytichadou <samytichadou@gmail.com>
2020-09-06 23:47:54 +0200

    first iteration of create manifest operator

--- op_create_manifest.py
+++ op_create_manifest.py
@@ -3,7 +3,7 @@ import os
 import string
 import random
 
-from .json_functions import read_json
+from .json_functions import read_json, create_json_file
 
 
 # return direct subfolders
@@ -71,6 +71,8 @@ class ANTEMPLATES_OT_create_manifest(bpy.types.Operator):
 
     template_folder : bpy.props.StringProperty(name="Template Folder")
 
+    output_path : bpy.props.StringProperty(name="Output Path")
+
     @classmethod
     def poll(cls, context):
         return True
@@ -84,19 +86,31 @@ class ANTEMPLATES_OT_create_manifest(bpy.types.Operator):
         layout = self.layout
         layout.prop(self, "github_url")
         layout.prop(self, "template_folder")
+        layout.prop(self, "output_path")
 
 
     def execute(self, context):
+        
+        # check and correct output path
 
-        categories = []
+        json_path = self.output_path
+
+        if not os.path.isdir(os.path.dirname(json_path)):
+            print("Incorrect Output Path")
+            return {'FINISHED'}
+
+        if not json_path.endswith(".json"):
+            print("ends")
+            json_path += ".json"
+            print(json_path)
 
         manifest_datas = initialize_json_manifest_datas()
         
         for folder in return_direct_subfolders(self.template_folder):
 
             # get categories
-            categories.append(os.path.basename(folder))
-            print(folder)
+            category = os.path.basename(folder)
+            manifest_datas["categories"].append({"name" : category})
 
             # get nodetrees
             for subfolder in return_direct_subfolders(folder):
@@ -108,12 +122,21 @@ class ANTEMPLATES_OT_create_manifest(bpy.types.Operator):
                         nodetree_datas = read_json(filepath)
 
                         # create json entry for the nodetree
+                        nodetree_datas["category"] = category
+                        manifest_datas["nodetrees"].append(nodetree_datas)
 
-                        for data in nodetree_datas:
-                            print(data)
-                            
+                        if nodetree_datas["blender_version"] not in manifest_datas["blender_versions"]:
+                            manifest_datas["blender_versions"].append({"name" : nodetree_datas["blender_version"]})
+
+                        if nodetree_datas["an_version"] not in manifest_datas["an_versions"]:
+                            manifest_datas["an_versions"].append({"name" : nodetree_datas["an_version"]})
 
                         break
 
+        print("Creating manifest")
+
+        print(manifest_datas)
+
+        create_json_file(manifest_datas, json_path)
 
         return {'FINISHED'}
