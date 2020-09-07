7052ed91ba428509c2376c293173354123869192
samytichadou <samytichadou@gmail.com>
2020-09-07 14:55:19 +0200

    syntax

--- gui.py
+++ gui.py
@@ -2,7 +2,7 @@ import bpy
 
 
 class ANTEMPLATES_PT_panel(bpy.types.Panel):
-    bl_idname = "ANTEMPLATES_PT_auto_execution_panel"
+    bl_idname = "ANTEMPLATES_PT_panel"
     bl_label = "Templates"
     bl_space_type = "NODE_EDITOR"
     bl_region_type = "UI"
