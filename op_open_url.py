324b8724c8de4acd8e0c88b587cdc01ed681488b
samytichadou <samytichadou@gmail.com>
2020-09-12 12:03:47 +0200

    deal with no url in open url op

--- op_open_url.py
+++ op_open_url.py
@@ -1,6 +1,10 @@
 import bpy
 import webbrowser
 
+
+from .global_variables import addon_print_prefix
+
+
 class ANTEMPLATES_OT_open_url(bpy.types.Operator):
     """Open URL in web browser"""
     bl_idname = "antemplates.open_url"
@@ -18,5 +22,9 @@ class ANTEMPLATES_OT_open_url(bpy.types.Operator):
             return True
 
     def execute(self, context):
-        webbrowser.open(self.url)
+        if self.url == "":
+            print(addon_print_prefix + "No URL to Open")
+        else:
+            webbrowser.open(self.url)
+            print(addon_print_prefix + "Opening URL in Web Browser : %s" % self.url)
         return {'FINISHED'}
