b66cfbdabc7fdd76d70eea800175e70399c0d6ea
samytichadou <samytichadou@gmail.com>
2020-09-13 17:06:25 +0200

    move files to old

--- global_variables.py
+++ global_variables.py
@@ -8,7 +8,7 @@ nodetree_infos = "nodetree_infos.json"
 # github
 raw_github_prefix = "https://raw.githubusercontent.com"
 manifest_url = "https://raw.githubusercontent.com/samytichadou/animation_nodes_examples/master/library/manifest.json"
-global_k_url = "https://raw.githubusercontent.com/samytichadou/an_template_addon/master/global_k.py"
+global_k_url = "https://raw.githubusercontent.com/samytichadou/an_template_addon/master/global_k.json"
 
 # global k
 global_k_filepath = os.path.join(os.path.dirname(__file__), "global_k.json")
