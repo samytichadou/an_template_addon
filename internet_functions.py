6312beb7ef1f64e07dca8c849ee7c51fe8708ce2
samytichadou <samytichadou@gmail.com>
2020-10-21 15:21:49 +0200

    remove debug prints

--- internet_functions.py
+++ internet_functions.py
@@ -1,6 +1,6 @@
 def is_connected(host="8.8.8.8", port=53, timeout=3):
     import socket
-    print("Alternative Internet Connection Check")
+
     try:
         socket.setdefaulttimeout(timeout)
         socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
