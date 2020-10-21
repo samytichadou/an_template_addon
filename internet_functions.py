9eecccb70f3aa742705aae738db31d6750254226
samytichadou <samytichadou@gmail.com>
2020-10-21 12:53:34 +0200

    alternative internet check

--- internet_functions.py
+++ internet_functions.py
@@ -1,14 +1,13 @@
-def is_connected():
-    #import socket
-    #try:
-        # connect to the host -- tells us if the host is actually reachable
-    #    socket.create_connection(("1.1.1.1", 53))
-    #    return True
-    #except OSError:
-    #    pass
-    #return False
-    print("Bypassing Internet Connection Check")
-    return True
+def is_connected(host="8.8.8.8", port=53, timeout=3):
+    import socket
+    print("Alternative Internet Connection Check")
+    try:
+        socket.setdefaulttimeout(timeout)
+        socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
+        return True
+    except socket.error as ex:
+        print(ex)
+        return False
     
 
 def read_online_json(url):
