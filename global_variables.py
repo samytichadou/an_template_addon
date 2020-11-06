37f7ba6192942a1b0f5d85cda6692e34b9a4e5ae
samytichadou <samytichadou@gmail.com>
2020-11-06 16:21:04 +0100

    global variables syntax, adding newfeed variable

--- global_variables.py
+++ global_variables.py
@@ -8,19 +8,23 @@ image_preview = "image_preview.png"
 readme = "readme.md"
 nodetree_infos = "nodetree_infos.json"
 
-# github libraries
-manifest_url = "https://raw.githubusercontent.com/samytichadou/animation_nodes_examples/master/library/manifest.json"
-global_k_url = "https://raw.githubusercontent.com/samytichadou/animation_nodes_examples/master/library/global_k.json"
-addon_version_url = "https://raw.githubusercontent.com/samytichadou/an_template_addon/master/addon_version.json"
-
-# global k
-global_k_filepath = os.path.join(os.path.dirname(__file__), "global_k.json")
-
 # common
 addon_print_prefix = "AN Templates --- "
 
-# files
+# file names
 manifest_file = "manifest.json"
+global_k_file = "global_k.json"
+newsfeed_file = "newsfeed.json"
+
+# global k
+global_k_filepath = os.path.join(os.path.dirname(__file__), global_k_file)
 
 # mail
 submission_mail_subject = "Submission - "
+
+# github libraries
+an_lib_github = "https://raw.githubusercontent.com/samytichadou/animation_nodes_examples/master/library/"
+manifest_url = an_lib_github + manifest_file
+global_k_url = an_lib_github + global_k_file
+news_feed_url = an_lib_github + newsfeed_file
+addon_version_url = "https://raw.githubusercontent.com/samytichadou/an_template_addon/master/addon_version.json"
