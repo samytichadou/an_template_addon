493910f9dd0dc9c82de603c7b2a6fd2dc65b6148
samytichadou <samytichadou@gmail.com>
2020-11-06 16:22:17 +0100

    syntax

--- global_variables.py
+++ global_variables.py
@@ -27,4 +27,5 @@ an_lib_github = "https://raw.githubusercontent.com/samytichadou/animation_nodes_
 manifest_url = an_lib_github + manifest_file
 global_k_url = an_lib_github + global_k_file
 news_feed_url = an_lib_github + newsfeed_file
+
 addon_version_url = "https://raw.githubusercontent.com/samytichadou/an_template_addon/master/addon_version.json"
